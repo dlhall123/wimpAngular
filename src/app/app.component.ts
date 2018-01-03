47974385da11b1f50edff6de4ac6409a543c8c6d
David Hall <david.hall@libertymutual.com>
2018-01-02 21:12:33 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WimpService } from './wimp.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  movies;
+  actors;
+  constructor(private wimpService: WimpService){}
+  
+  getMovies(){
+      this.wimpService.getRecords("movies")
+        .subscribe(moviesFromAPI => {
+          this.movies = moviesFromAPI;
+        })
+  }
+  
+  getActors() {
+    this.wimpService.getRecords("actors")
+      .subscribe(actorsFromAPI => {
+        this.actors = actorsFromAPI;
+      })
+  }
+
+  submitMovie(movieForm: NgForm){
+    console.log(movieForm.value);
+    this.wimpService.addRecord("movies", movieForm.value)
+    .subscribe(
+      moviesInfo => {
+      console.log(moviesInfo)
+      this.getMovies();
+      });
+  }
+  submitActor(actorForm: NgForm) {
+    console.log(actorForm.value);
+    this.wimpService.addRecord("actors", actorForm.value)
+      .subscribe(
+      actorsInfo => {
+        console.log(actorsInfo)
+        this.getActors();
+      });
+  }
+
+  ngOnInit() {
+    this.getMovies();
+    this.getActors();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WimpService } from './wimp.service';
+
 
 
 @NgModule({
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [WimpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
